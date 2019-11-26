65fd88d80c4f08af365b40552c15121bc0da3cc8
Engr-hasan <hasankhan.cse@gmail.com>
2019-11-26 20:30:50 +0600

    Add MiddleWare

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Redirect;
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (Auth::check()) {   // && Auth::user()->name
+            return $next($request);
+        }else{
+            return redirect()->route('login');
+        }
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -8,23 +8,26 @@ Auth::routes();
 
 Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/list-sliders', 'HomeSliderController@listSlider');
-Route::get('/add-slider', 'HomeSliderController@addSlider');
-Route::post('/store-slider', 'HomeSliderController@storeSlider');
-Route::get('/edit-slider/{id}', 'HomeSliderController@editSlider');
-Route::post('/update-slider/{id}', 'HomeSliderController@updateSlider');
-Route::get('/delete-slider/{id}', 'HomeSliderController@deleteSlider');
+Route::group(['middleware'=>['auth']], function
+(){
+	Route::get('/list-sliders', 'HomeSliderController@listSlider');
+	Route::get('/add-slider', 'HomeSliderController@addSlider');
+	Route::post('/store-slider', 'HomeSliderController@storeSlider');
+	Route::get('/edit-slider/{id}', 'HomeSliderController@editSlider');
+	Route::post('/update-slider/{id}', 'HomeSliderController@updateSlider');
+	Route::get('/delete-slider/{id}', 'HomeSliderController@deleteSlider');
 
-Route::get('/list-clients', 'ClientController@listClient');
-Route::get('/add-client', 'ClientController@addClient');
-Route::post('/store-client', 'ClientController@storeClient');
-Route::get('/edit-client/{id}', 'ClientController@editClient');
-Route::post('/update-client/{id}', 'ClientController@updateClient');
-Route::get('/delete-client/{id}', 'ClientController@deleteClient');
+	Route::get('/list-clients', 'ClientController@listClient');
+	Route::get('/add-client', 'ClientController@addClient');
+	Route::post('/store-client', 'ClientController@storeClient');
+	Route::get('/edit-client/{id}', 'ClientController@editClient');
+	Route::post('/update-client/{id}', 'ClientController@updateClient');
+	Route::get('/delete-client/{id}', 'ClientController@deleteClient');
 
-Route::get('/list-galleries', 'GalleryController@listGallery');
-Route::get('/add-gallery', 'GalleryController@addGallery');
-Route::post('/store-gallery', 'GalleryController@storeGallery');
-Route::get('/edit-gallery/{id}', 'GalleryController@editGallery');
-Route::post('/update-gallery/{id}', 'GalleryController@updateGallery');
-Route::get('/delete-gallery/{id}', 'GalleryController@deleteGallery');
+	Route::get('/list-galleries', 'GalleryController@listGallery');
+	Route::get('/add-gallery', 'GalleryController@addGallery');
+	Route::post('/store-gallery', 'GalleryController@storeGallery');
+	Route::get('/edit-gallery/{id}', 'GalleryController@editGallery');
+	Route::post('/update-gallery/{id}', 'GalleryController@updateGallery');
+	Route::get('/delete-gallery/{id}', 'GalleryController@deleteGallery');
+});
